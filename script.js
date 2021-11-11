8f5544c1c7711c30c9ba01569f7320ddad595759
khatrigautam2001 <69420108+khatrigautam2001@users.noreply.github.com>
2021-11-11 10:12:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const tempLoad = () => {
+    let temp = document.getElementById('temp');
+    temp.innerHTML = "&#xf2cb";
+    temp.style.color='#f8b627';
+
+    setTimeout(() => {
+        temp.innerHTML="&#xf2ca";
+        temp.style.color='#f8b627';
+    },1000);
+
+    setTimeout(() => {
+        temp.innerHTML="&#xf2c9";
+        temp.style.color='#f8b627';
+    },2000);
+
+    setTimeout(() => {
+        temp.innerHTML="&#xf2c8";
+        temp.style.color='#f8b627';
+    },3000);
+
+    setTimeout(() => {
+        temp.innerHTML="&#xf2c7";
+        temp.style.color='#d63031';
+    },4000);
+}
+
+tempLoad();
+
+setInterval(tempLoad, 5000);
